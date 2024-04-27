29823f2c37a9f81bbdf49d3d4666db93c72e0111
valeriy korovin <1k9o9r5@gmail.com>
2024-04-27 11:30:00 +0300

    HW60_v2

--- js/solution.js
+++ js/solution.js
@@ -1,4 +1,20 @@
 (function () {
   const ul = document.getElementById('ulId');
   const li = ul.getElementsByTagName('li');
+
+  const attributeUl = Array.from(ul.attributes);
+  const attrValue = attributeUl.map(item => item.value);
+  console.log(attrValue);
+
+  const attrName = attributeUl.map(item => item.name);
+  console.log(attrName);
+
+  const lastLi = li[li.length - 1];
+  lastLi.innerHTML = 'Привет меня зовут Джони';
+  lastLi.textContent = 'Привет меня зовут Валерий';
+
+  const firstLi = li[0];
+  firstLi.setAttribute('data-my-name', 'Valeriy');
+
+  ul.removeAttribute('data-dog-tail');
 }());
