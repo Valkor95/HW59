e6cc324af990d9e5a1f67990e42e8dfe38cd7253
valeriy korovin <1k9o9r5@gmail.com>
2024-06-17 00:45:20 +0300

    MVC_v4

--- js/Controller.js
+++ js/Controller.js
@@ -2,21 +2,13 @@ import Model from "./Model.js";
 import View from "./View.js";
 
 const Controller = {
-    // removeHandler (event)  {
-    //      const target = event.target.closest('li');
-    //      const postId = Number(target.getAttribute('data-id'));
-    //      const data = getData();
-    //      const indexItem = data.findIndex(item =>
-    //          item.id === postId);
-    //      data.splice(indexItem, 1);
-    //      localStorage.setItem(dataKey, JSON.stringify(data))
-    //      target.remove()
-    //  },
+
 
     loadedHandler () {
         Model.checkData();
         const data = Model.getData();
-        View.createTemplate(data)
+        View.createTemplate(data);
+        View.removeHandler(data)
     },
 
     init(){
--- js/View.js
+++ js/View.js
@@ -12,10 +12,21 @@ const View = {
                         <p>${item.body}</p>`;
             ul.append(li)
 
-            // ul.addEventListener('click', removeHandler);
+            ul.addEventListener('click', this.removeHandler);
         })
         this.divContainer.append(ul)
     },
+
+    removeHandler (event)  {
+         const target = event.target.closest('li');
+         const postId = Number(target.getAttribute('data-id'));
+         const data = getData();
+         const indexItem = data.findIndex(item =>
+             item.id === postId);
+         data.splice(indexItem, 1);
+         localStorage.setItem(dataKey, JSON.stringify(data))
+         target.remove()
+     },
 
 }
 
